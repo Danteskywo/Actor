e20d4c3dd013b49d8fcd2aa0560cab89bfed52ae
Webis <akobyanrobert8@gmail.com>
2026-01-31 22:08:27 +0300

    Исправлено создание движка и асинхронной локальной сессии

--- app/database.py
+++ app/database.py
@@ -13,10 +13,12 @@ DB_PASSWORD = os.getenv('DB_PASSWORD', '77777')
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-AsyncSessionLocal = None
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 class Base(DeclarativeBase):
     pass
 
 async def get_async_session() -> AsyncSession:
-    return None
+    async with AsyncSessionLocal() as session:
+        yield session
