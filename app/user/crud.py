5d77311f92d7bb56ea745c4fc9256d9ff3381712
Webis <akobyanrobert8@gmail.com>
2026-01-31 12:18:31 +0300

    исправлена двойная проверка существования пользователя.

--- app/user/crud.py
+++ app/user/crud.py
@@ -1,15 +1,11 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
 import bcrypt
-
-from .models import User
-from .scheme import UserRegister
-
+from app.user.models import User
+from app.user.scheme import UserRegister
 
 class UserCRUD:
     async def create_user(self, session: AsyncSession, user_data: UserRegister) -> User:
-        
         result = await session.execute(
             select(User).where(
                 (User.username == user_data.username) |
@@ -17,22 +13,18 @@ class UserCRUD:
             )
         )
         existing = result.scalar_one_or_none()
-
-        if existing.scalar_one_or_none():
+        if existing:
             raise ValueError("Пользователь уже существует!")
         
-        # Хэшируем пароль
-
         hashed_password = bcrypt.hashpw(
             user_data.password.encode('utf-8'),
             bcrypt.gensalt()
         ).decode('utf-8')
 
-        # Создаем пользователя!
         user = User(
             username=user_data.username,
-            email= user_data.email,
-            hashed_password = hashed_password,
+            email=user_data.email,
+            hashed_password=hashed_password,
         )
         session.add(user)
         await session.commit()
@@ -46,16 +38,13 @@ class UserCRUD:
         )
         return result.scalar_one_or_none()
 
-
     @staticmethod
     async def authenticate_user(session: AsyncSession, username: str, password: str) -> User:
         user = await UserCRUD.get_user_by_username(session, username)
-
         if not user:
             return None
         if not user.is_active:
             return None
         if not user.check_password(password):
             return None
-        return user        
-        
+        return user
